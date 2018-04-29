e549ef0142f41d47252eeb6b4bd140b620c00f29
Philippe Joulot <philippe.joulot@laposte.net>
2018-04-29 14:24:14 +0000

    Some adjustments + integrate the copyright

--- scripts/fix-perms.sh
+++ scripts/fix-perms.sh
@@ -4,6 +4,11 @@
 ENV=dev
 . `dirname $0`/env.sh
 
+# Set permissions for www.
+chown -R $USER:$APACHE_USER $WWW_PATH
+chmod -R 640 $WWW_PATH
+chmod -R +X $WWW_PATH
+
 # Set permissions for all project.
 chown -R $USER:$GROUP $PROJECT_PATH
 chmod -R 644 $PROJECT_PATH/
@@ -22,17 +27,12 @@ chmod +x $PROJECT_PATH/scripts/*
 # chmod +x $PROJECT_PATH/conf/solr/*
 
 # Set permissions for media.
-if [ -d "$PROJECT_PATH/sites/default/files" ]
+if [ -d "$PROJECT_PATH/web/sites/default/files" ]
 then
-    chown -R $USER:$APACHE_USER $PROJECT_PATH/sites/default/files
-    chmod -R 660 $PROJECT_PATH/sites/default/files
-    chmod -R +X $PROJECT_PATH/sites/default/files
+    chown -R $USER:$APACHE_USER $PROJECT_PATH/web/sites/default/files
+    chmod -R 775 $PROJECT_PATH/web/sites/default/files
+    chmod -R +X $PROJECT_PATH/web/sites/default/files
 fi
-
-# Set permissions for www.
-chown -R $USER:$APACHE_USER $WWW_PATH
-chmod -R 640 $WWW_PATH
-chmod -R +X $WWW_PATH
 
 # Set permissions for log.
 if [ -f "/var/log/crontab.log" ]
