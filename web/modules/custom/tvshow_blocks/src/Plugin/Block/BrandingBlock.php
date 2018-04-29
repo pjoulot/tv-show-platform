d949d644c392eb0c1933bcd2124aed3c837b1e2d
Philippe Joulot <philippe.joulot@laposte.net>
2018-04-29 22:40:15 +0000

    Adding theme ressources + integrate the branding block

---
+++ web/modules/custom/tvshow_blocks/src/Plugin/Block/BrandingBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\tvshow_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Branding' Block
+ *
+ * @Block(
+ *   id = "tvshow_branding_block",
+ *   admin_label = @Translation("TvShow Branding Block"),
+ * )
+ */
+class BrandingBlock extends BlockBase implements BlockPluginInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $config = \Drupal::service('config.factory')->getEditable('system.site');
+
+    return array(
+      '#theme' => 'tvshow_branding_block',
+      '#site_slogan' => $config->get('slogan', ''),
+      '#site_url_name' => $config->get('site_url_name', ''),
+    );
+  }
+}
