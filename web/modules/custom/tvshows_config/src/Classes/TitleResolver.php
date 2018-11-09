765a6b33946e594c2a590ae7a0542d8eecd0ba2b
Philippe Joulot <philippe.joulot@laposte.net>
2018-11-09 00:09:45 +0000

    Fix the title of the views news by tag

--- web/modules/custom/tvshows_config/src/Classes/TitleResolver.php
+++ web/modules/custom/tvshows_config/src/Classes/TitleResolver.php
@@ -4,6 +4,9 @@ namespace Drupal\tvshows_config\Classes;
 
 use Drupal\Component\Utility\Xss;
 use Drupal\user\UserInterface;
+use Drupal\views\Views;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\Component\Utility\Html;
 
 /**
  * Class to override route titles.
@@ -25,4 +28,78 @@ class TitleResolver {
     return $user ? ['#markup' => $user_title, '#allowed_tags' => Xss::getHtmlTagList()] : '';
   }
 
+  /**
+   * Route title callback.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The user account.
+   *
+   * @return string|array
+   *   The user account name as a render array or an empty string if $user is
+   *   NULL.
+   */
+  public function newsViewsTitle() {
+    $title = NULL;
+    $route = \Drupal::routeMatch()->getRouteObject();
+    $parameters = \Drupal::routeMatch()->getParameters();
+    if ($route) {
+      $view_id = $parameters->get('view_id');
+      $display_id = $parameters->get('display_id');
+      $arg_0 = $parameters->get('arg_0');
+      if (!empty($view_id) && !empty($display_id) && !empty($arg_0)) {
+        $view = Views::getView($view_id);
+        if ($view) {
+          $args = [$arg_0];
+          $view->setArguments($args);
+          $view->setDisplay($display_id);
+          $view->preExecute();
+
+          // Check if the view is filtering by a bundle.
+          $filtered_bundles = ($bundles = $view->argument['name']->options['validate_options']['bundles']) ? array_keys($bundles) : [];
+          $term = $this->getTaxonomyTermByName($arg_0, $filtered_bundles);
+          if (!empty($term)) {
+            // @todo Use a dependency injection to use the translation service.
+            $title = t('News @term_name', array('@term_name' => $term->getName()));
+          }
+          else {
+            $title = $view->getTitle();
+          }
+        }
+      }
+    }
+    return $title ? ['#markup' => $title, '#allowed_tags' => Xss::getHtmlTagList()] : '';
+  }
+
+  /**
+   * Function to get term by name.
+   *
+   * @param string $name
+   * The searched name.
+   * @param array $vids
+   *   The vocabularies to search.
+   *
+   * @return NULL|object
+   *   The found term object or NULL if nothing is found.
+   */
+  public function getTaxonomyTermByName($name, $vids = []) {
+    // @todo Move this function inside a separated service.
+    $found_term = NULL;
+
+    $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery();
+    if (!empty($vids)) {
+      $query->condition('vid', $vids, 'IN');
+    }
+    $tids = $query->execute();
+    $terms = Term::loadMultiple($tids);
+
+    foreach ($terms as $term) {
+      $term_name = Html::getId(strtolower($term->getName()));
+      if ($term_name === $name) {
+        $found_term = $term;
+        break;
+      }
+    }
+
+    return $found_term;
+  }
 }
--- web/modules/custom/tvshows_config/src/Routing/RouteSubscriber.php
+++ web/modules/custom/tvshows_config/src/Routing/RouteSubscriber.php
@@ -14,12 +14,29 @@ class RouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection) {
-    // Override title of the user page.
-    if ($route = $collection->get('entity.user.canonical')) {
-      $defaults = $route->getDefaults();
-      $defaults['_title_callback'] = 'Drupal\tvshows_config\Classes\TitleResolver::userTitle';
-      $route->setDefaults($defaults);
+    // Override title for some routes.
+    $routes = [
+      'entity.user.canonical' => 'userTitle',
+      'view.taxonomy_term.page_1' => 'newsViewsTitle'
+    ];
+    foreach ($routes as $route_name => $title_function) {
+      $route = $collection->get($route_name);
+      $this->setRouteTitle($route, $title_function);
     }
+  }
+
+  /**
+   * Function to override the title of an existing route.
+   *
+   * @param object $route
+   *   The drupal route object.
+   * @param string $title_function
+   *   The custom function to call to generate the title.
+   */
+  protected function setRouteTitle($route, $title_function) {
+    $defaults = $route->getDefaults();
+    $defaults['_title_callback'] = 'Drupal\tvshows_config\Classes\TitleResolver::' . $title_function;
+    $route->setDefaults($defaults);
   }
 
 }
